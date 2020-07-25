f1868c78ec04092e84020f9f30f499b774412f13
GonzaloSaad <saad.gonzalo.ale@gmail.com>
2020-07-25 15:56:37 -0300

    Add google challenge to Route53

--- terraform/base/route53_root_zone.tf
+++ terraform/base/route53_root_zone.tf
@@ -15,6 +15,15 @@ resource "aws_route53_record" "compute_subdomain_record" {
   records = aws_route53_zone.compute_zone.name_servers
 }
 
+resource "aws_route53_record" "root_google_record" {
+  zone_id = aws_route53_zone.root_zone.zone_id
+  name = "universy.app"
+  type = "TXT"
+  ttl = "60"
+  records = ["insert_google_challenge_here"]
+}
+
+
 resource "aws_route53_record" "root_dns_challenge" {
   name    = aws_acm_certificate.root_certificate.domain_validation_options[0].resource_record_name
   type    = aws_acm_certificate.root_certificate.domain_validation_options[0].resource_record_type
